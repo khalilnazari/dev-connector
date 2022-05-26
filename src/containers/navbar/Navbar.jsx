a0e1045a4a78c742405a82e14588dad56cfc8b9d
khalilnazari <khalil.deve@gmail.com>
2022-05-26 21:12:14 +0800

    Navbar code modified

--- src/containers/navbar/Navbar.jsx
+++ src/containers/navbar/Navbar.jsx
@@ -7,9 +7,7 @@ const Navbar = () => {
     <nav className="navbar">
       <div className="container">
         {/* logo */}
-        <h1>
-          <a href="index.html" className='logo-link'><FaCodeBranch /> DevConnector</a>
-        </h1>
+        <h1><a href="index.html" className='logo-link'><FaCodeBranch /> DevConnector</a></h1>
 
         {/* linkes */}
         <ul>
@@ -17,17 +15,15 @@ const Navbar = () => {
           <li><a href="posts.html" className='nav-link'>Posts</a></li>
           
           <li>
-            <a href="dashboard.html" title="Dashboard" className='nav-link'
-              >
-              <span className="hide-sm">Dashboard</span></a
-            >
+            <a href="dashboard.html" title="Dashboard" className='nav-link'>
+              <span className="hide-sm">Dashboard</span>
+            </a>
           </li>
 
           <li>
-            <a href="login.html" title="Logout" className='nav-link'>
-              
-              <span className="hide-sm">Logout</span></a
-            >
+            <a href="login.html" title="Logout" className='nav-link'>  
+              <span className="hide-sm">Logout</span>
+            </a>
           </li>
         </ul>
       </div>
