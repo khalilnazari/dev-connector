b36b2e078e80562a5362a9aaf4d5f681515673b9
khalilnazari <khalil.deve@gmail.com>
2022-05-30 20:35:16 +0800

    Dashboard is developed

--- src/containers/dashboard/Dashboard.jsx
+++ src/containers/dashboard/Dashboard.jsx
@@ -1,8 +1,94 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './dashboard.scss'
+import {ImProfile} from 'react-icons/im'
+import {FaToolbox, FaGraduationCap} from 'react-icons/fa'
+
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <section className="dashboard">
+      <div className="container">
+        <h1 className="page-title">Dashboard</h1>
+        <p className='lead'>Welcome John Smith</p>
+
+        {/* edit button */}
+        <div className='profile-action-section'>
+          <Link className='profile-action-btn' to="/create-profile"><ImProfile /> Edit Profile</Link>
+          <Link className='profile-action-btn' to="/add-experience"><FaToolbox /> Add Experience</Link>
+          <Link className='profile-action-btn' to="/add-education"><FaGraduationCap />Add Education</Link>
+        </div>
+
+
+        {/* section */}
+        <h2 className='section-title'>Experience Credentials</h2>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Title</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Microsoft	</td>
+              <td>Senior Developer</td>
+              <td>
+                <span>Oct 2011 - Current</span>
+                <button className='delete-btn'>Delete</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Microsoft	</td>
+              <td>Senior Developer</td>
+              <td>
+                <span>Oct 2011 - Current</span>
+                <button className='delete-btn'>Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+
+        <h2 className='section-title'>Education Credentials</h2>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Company</th>
+              <th>Title</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Microsoft	</td>
+              <td>Senior Developer</td>
+              <td>
+                <span>Oct 2011 - Current</span>
+                <button className='delete-btn'>Delete</button>
+              </td>
+            </tr>
+
+            <tr>
+              <td>Microsoft	</td>
+              <td>Senior Developer</td>
+              <td>
+                <span>Oct 2011 - Current</span>
+                <button className='delete-btn'>Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <button className='delete-account-btn'>Delete My Account</button>
+      </div>
+    </section>
   )
 }
 
