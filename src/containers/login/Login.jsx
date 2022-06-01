4c274eacffd97cf236a43d495dae1bdf94bf0c6b
khalilnazari <khalil.deve@gmail.com>
2022-06-01 22:18:49 +0800

    Login and signup page are developed

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './login.scss'
+import {FaUser} from 'react-icons/fa';
+import { PrimaryButton } from '../../components/buttons/Buttons';
+import { Link } from 'react-router-dom';
+
 
 const Login = () => {
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const email = localStorage.getItem('devc-email'); 
+    const password = localStorage.getItem('devc-password'); 
+
+    console.log(email, password)
+  }
+
   return (
-    <div>Login</div>
+    <section className='login-section'>
+      <div className="container">
+        <h2 className="page-title">Log In</h2>
+        <div className="lead"><FaUser /> Sign into your account</div>
+
+        <form action="" method='POST' onSubmit={handleFormSubmit}>
+          <input type="email" name='email' placeholder='Email Address'/>
+          <input type="password" name='password' placeholder='Password'/>
+          <PrimaryButton text='Submit' bgColor="green" textColor="white" type='submit'/>
+        </form>
+
+        <p className='login-link'>Don't have an account? <Link to='/register'>Sign up</Link></p>
+      </div>
+    </section>
   )
 }
 
--- src/containers/register/Register.jsx
+++ src/containers/register/Register.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { PrimaryButton } from '../../components/buttons/Buttons'
 import './register.scss'
+import {FaUser} from 'react-icons/fa'
+
 
 const Register = () => {
+
+  const [name, setName] = useState(); 
+  const [email, setEmail] = useState(); 
+  const [password, setPassword] = useState(); 
+  const [passwordConfirm, setPasswordConfirm] = useState(); 
+  const [inputError, setInputError]= useState('');
+
+  const onNameChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const onEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const onPasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const onPasswordConfirmChange = (e) => {
+    setPasswordConfirm(e.target.value)
+  }
+  
+
+  // form submit
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    let passErr = false; 
+    if(password !== passwordConfirm) {
+      console.log('err pass')
+      setInputError('Password does not match!')
+      passErr = true; 
+    } 
+    
+    if(!passErr) {
+      
+      localStorage.setItem('devc-name', name); 
+      localStorage.setItem('devc-email', email); 
+      localStorage.setItem('devc-password', password); 
+    }
+  }
+
+  // jsx
   return (
-    <div>Register</div>
+    <section className='register-section'>
+      <div className="container">
+        <h2 className="page-title">Sign Up</h2>
+        <div className="lead"><FaUser /> create your account</div>
+
+        <p className='error-text'>{inputError}</p>
+        <form action="" method='POST' onSubmit={handleFormSubmit}>
+          <input type="text" name='name' placeholder='Name' onChange={e => onNameChange(e)}/>
+          <input type="email" name='email' placeholder='Email Address' onChange={e => onEmailChange(e)}/>
+          <input type="password" name='password' placeholder='Password' onChange={ e=> onPasswordChange(e) }/>
+          <input type="password" name='passwordConfirm' placeholder='Confirm Password' onChange={e => onPasswordConfirmChange(e)}/>
+          <PrimaryButton text='Submit' bgColor="green" textColor="white" type='submit'/>
+        </form>
+
+        <p className='login-link'>Have an account <Link to='/login'>Login</Link></p>
+      </div>
+    </section>
   )
 }
 
